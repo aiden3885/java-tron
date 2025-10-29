025a7c2d905f87dbbc79465dc4576306eedae0bd
hanliang <bualua61@163.com>
2025-10-29 15:08:11 +0800

    feat(trans): add logger

--- framework/src/main/java/org/tron/core/services/http/FilterInternalTransactionServlet.java
+++ framework/src/main/java/org/tron/core/services/http/FilterInternalTransactionServlet.java
@@ -40,7 +40,9 @@ public class FilterInternalTransactionServlet extends InternalTransServlet {
         IRevokingDB revokingDB = chainBaseManager.getTransactionRetStore().getRevokingDB();
         while (sc.hasNextLine()) {
             String str = sc.next();
-            String[] split = str.split("\t");
+            logger.info(str);
+            String[] split = str.trim().split("\t");
+            logger.info(String.format("%d", split.length));
             Long blockNumber = Long.parseLong(split[0]);
             logger.info("blockNumber is " + blockNumber);
             String trxs =split[1];
